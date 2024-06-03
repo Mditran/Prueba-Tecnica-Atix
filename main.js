635be0d1a4596af3828db5f88db091ba1cb9ddb8
Mditran <micheldiaz@unicauca.edu.co>
2024-06-03 17:43:52 -0500

    Version final

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import express from 'express';
 import generatePdf from './pdf/generatePDF.js';
 import path from 'path';
 
-
 const app = express();
 const port = 3005;
 
@@ -22,37 +21,17 @@ app.post('/generate-pdf', async (req, res) => {
             captchaText: req.body.captchaText,
         });
 
-        res.status(200)
+/*         res.status(200)
             .set({
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Credentials': true,
                 'Content-Type': 'application/pdf',
             })
-            .end(pdfBuffer);
+            .end(pdfBuffer); */
     } catch (error) {
         console.error('Error al generar el PDF:', error);
         res.status(500).json({ error: 'Error al generar el PDF' });
     }
-});
-
-app.get('/download-img/:numeroDocumento', (req, res) => {
-    const filePath = path.join(__dirname, './img', `${req.params.numeroDocumento}.png`);
-    res.download(filePath, (err) => {
-        if (err) {
-            console.error('Error al descargar la imagen:', err);
-            res.status(500).json({ error: 'Error al descargar la imagen' });
-        }
-    });
-});
-
-app.get('/download-html/:numeroDocumento', (req, res) => {
-    const filePath = path.join(__dirname, './html', `${req.params.numeroDocumento}.html`);
-    res.download(filePath, (err) => {
-        if (err) {
-            console.error('Error al descargar el HTML:', err);
-            res.status(500).json({ error: 'Error al descargar el HTML' });
-        }
-    });
 });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
